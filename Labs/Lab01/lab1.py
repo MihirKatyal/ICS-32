b911a24494e59e14698322b1a05de0c41e8dc0c6
Mihir Katyal <mkatyal@uci.edu>
2024-01-12 17:36:42 -0800

    Created inputs for function

---
+++ Labs/Lab01/lab1.py
@@ -0,0 +1,20 @@
+# lab1.py
+
+# Starter code for lab 1 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+# Please see the README in this repository for the requirements of this lab exercise
+
+# Mihir Katyal
+# mkatyal@uci.edu
+# 19099879
+
+# Welcome message
+print("Welcome to ICS 32 PyCalc!")
+
+# Input from the user
+operand1 = float(input("Enter your first operand: "))
+operand2 = float(input("Enter your second operand: "))
+operator = input("Enter your desired operator (+, -, or x): ")
+
+
